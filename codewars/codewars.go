2b1b8c6b7bb1093ea166592b722820fa64e525c0
nabinno <nab@blahfe.com>
2019-08-06 07:37:37 +0900

    Initial commit.

---
+++ codewars/codewars.go
@@ -0,0 +1,26 @@
+package codewars
+
+import (
+	"strings"
+)
+
+func main() {
+
+}
+
+// Points a game in CodeWar
+// @param {[]string} game - []string{"1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"}
+func Points(game []string) int {
+	var totalPoint int
+	for _, g := range game {
+		h := strings.Split(g, ":")
+		if h[0] > h[1] {
+			totalPoint += 3
+		} else if h[0] < h[1] {
+			totalPoint += 0
+		} else if h[0] == h[1] {
+			totalPoint++
+		}
+	}
+	return totalPoint
+}
---
+++ leetcode/leetcode.go
@@ -0,0 +1,10 @@
+package leetcode
+
+func main() {
+
+}
+
+// Defanging an IP Address in LeetCode (1108)
+func defangIPaddr(address string) string {
+
+}
