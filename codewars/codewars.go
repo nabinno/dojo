7a4210ffb455dcb92c6adca03f8c13235d5b253e
nabinno <nab@blahfe.com>
2019-08-06 13:46:45 +0900

    Submit LeetCode 771.

--- codewars/codewars.go
+++ codewars/codewars.go
@@ -1,4 +1,4 @@
-package codewars
+package main
 
 import (
 	"strings"
--- leetcode/leetcode.go
+++ leetcode/leetcode.go
@@ -1,10 +1,30 @@
-package leetcode
+package main
+
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-
+	j := "aA"
+	s := "aAAbbbb"
+	rc := numJewelsInStones(j, s)
+	fmt.Println(rc)
 }
 
-// Defanging an IP Address in LeetCode (1108)
+// Defanging an IP Address (1108)
 func defangIPaddr(address string) string {
+	return strings.Replace(address, ".", "[.]", -1)
+}
 
+// Jewels and Stones (771)
+func numJewelsInStones(J string, S string) int {
+	var gems []string
+
+	for i := 0; i < len(S); i++ {
+		if strings.Contains(J, string(S[i])) {
+			gems = append(gems, string(S[i]))
+		}
+	}
+	return len(gems)
 }
