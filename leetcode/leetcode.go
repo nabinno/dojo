ceb05b696f384172964853caa9df9a31697130f7
nabinno <nab@blahfe.com>
2019-08-08 21:22:25 +0900

    Update LeetCode.

--- leetcode/leetcode.go
+++ leetcode/leetcode.go
@@ -1,24 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"strings"
 )
 
 func main() {
-	j := "aA"
-	s := "aAAbbbb"
-	rc := numJewelsInStones(j, s)
-	fmt.Println(rc)
 }
 
-// Defanging an IP Address (1108)
-func defangIPaddr(address string) string {
+// DefangIPaddr (1108)
+func DefangIPaddr(address string) string {
 	return strings.Replace(address, ".", "[.]", -1)
 }
 
-// Jewels and Stones (771)
-func numJewelsInStones(J string, S string) int {
+// NumJewelsInStones (771)
+// @example
+//    ```
+//    j := "aA"
+//    s := "aAAbbbb"
+//    rc := NumJewelsInStones(j, s)
+//    fmt.Println(rc)
+//    ```
+func NumJewelsInStones(J string, S string) int {
 	var gems []string
 
 	for i := 0; i < len(S); i++ {
