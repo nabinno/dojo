43938fe0b0c62675031169329eb235958c94a47f
nabinno <nab@blahfe.com>
2019-08-08 21:21:52 +0900

    Submit ATOG moretypes 23.

--- a_tour_of_go/a_tour_of_go.go
+++ a_tour_of_go/a_tour_of_go.go
@@ -1,25 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
 
 func main() {
-	s := []int{2, 3, 5, 7, 11, 13}
-	printSlice(s)
-
-	// Slice the slice to give it zero length.
-	s = s[:0]
-	printSlice(s)
-
-	// Extend its length.
-	s = s[:4]
-	printSlice(s)
-
-	// Drop its first two values.
-	s = s[2:]
-	printSlice(s)
+	wc.Test(WordCount)
 }
 
-// printSlice (moretypes 11)
-func printSlice(s []int) {
+// WordCount (moretypes 23)
+// @example
+//   ```
+//   wc.Test(WordCount)
+//   ```
+func WordCount(s string) map[string]int {
+	rc := make(map[string]int)
+	for _, w := range strings.Fields(s) {
+		rc[w]++
+	}
+	return rc
+}
+
+// Pic (moretypes 18)
+// @example
+//   ```
+//   pic.Show(Pic)
+//   ```
+func Pic(dx, dy int) [][]uint8 {
+	rc := make([][]uint8, dy)
+	for y := 0; y < dy; y++ {
+		row := make([]uint8, dx)
+		for x := 0; x < dx; x++ {
+			row[x] = uint8(y)
+		}
+		rc[y] = row
+	}
+	return rc
+}
+
+// PrintSlice (moretypes 11)
+// @example
+//   ```
+//   s := []int{2, 3, 5, 7, 11, 13}
+//   printSlice(s)
+//   ```
+func PrintSlice(s []int) {
 	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 }
