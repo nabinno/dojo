1b910a0a563fff146d6f14da1489fcb0d5a00aff
nabinno <nab@blahfe.com>
2019-08-14 21:18:00 +0900

    Submit AtCoder ABC042A.

--- atcoder/atcoder.go
+++ atcoder/atcoder.go
@@ -3,11 +3,49 @@ package main
 import (
 	"fmt"
 	"math"
+	"sort"
+	"strings"
 )
 
 func main() {
-	rc := DetectEvenOrOdd()
+	rc := DetectHaikuLength2()
 	fmt.Println(rc)
+}
+
+// DetectHaikuLength2 (ABC042A)
+func DetectHaikuLength2() string {
+	var a, b, c string
+	_, _ = fmt.Scan(&a, &b, &c)
+	if strings.Count(a+b+c, "5") == 2 && strings.Count(a+b+c, "7") == 1 {
+		return "Yes"
+	}
+	return "No"
+}
+
+// DetectHaikuLength (ABC042A)
+func DetectHaikuLength() string {
+	checkEqualityOfSlices := func(a, b []int) bool {
+		if (a == nil) != (b == nil) {
+			return false
+		}
+		if len(a) != len(b) {
+			return false
+		}
+		for i := range a {
+			if a[i] != b[i] {
+				return false
+			}
+		}
+		return true
+	}
+	var a, b, c int
+	_, _ = fmt.Scanf("%d %d %d", &a, &b, &c)
+	ints := []int{a, b, c}
+	sort.Ints(ints)
+	if checkEqualityOfSlices(ints, []int{5, 5, 7}) {
+		return "Yes"
+	}
+	return "No"
 }
 
 // DetectEvenOrOdd (ABS ABC086A)
--- atcoder/atcoder_suite_test.go
+++ atcoder/atcoder_suite_test.go
@@ -11,3 +11,15 @@ func TestAtcoder(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Atcoder Suite")
 }
+
+// var _ = Describe("AtCoder ABC", func() {
+//	var _ = Describe("DetectHaikuLength (ABC042A)", func() {
+//		test := func(is []int, exp string) {
+//			Expect(DetectHaikuLength(is)).To(Equal(exp))
+//		}
+//		It("should handle the following cases", func() {
+//			test([]int{5, 5, 7}, "Yes")
+//			test([]int{7, 7, 5}, "No")
+//		})
+//	})
+// })
