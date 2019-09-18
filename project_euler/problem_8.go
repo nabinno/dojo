198eaca8dc49f64f03bfcd1dfdb709834fc29512
nabinno <nab@blahfe.com>
2019-09-19 07:31:20 +0900

    [wip]Submit Project Euler problem 8.

--- project_euler/problem_8.go
+++ project_euler/problem_8.go
@@ -2,5 +2,20 @@ package main
 
 // GetLargestProductInASeries ....
 func GetLargestProductInASeries(digit string) int {
+	var adjacentDigits []int
+
+	for _, d := range digit {
+		if int(d) == 0 || int(d) == 1 {
+			adjacentDigits = nil
+			continue
+		}
+
+		adjacentDigits = append(adjacentDigits, int(d))
+
+		if len(adjacentDigits) == 13 {
+			break
+		}
+	}
+
 	return 1
 }
