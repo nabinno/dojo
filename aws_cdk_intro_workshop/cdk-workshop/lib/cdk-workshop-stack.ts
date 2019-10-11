6c8121972b36082053fb5d4f04f3d0288a386ff1
nabinno <nab@blahfe.com>
2019-10-11 20:42:08 +0900

    AWS CDK Intro Workshop - Using construct libraries.

--- aws_cdk_intro_workshop/cdk-workshop/lib/cdk-workshop-stack.ts
+++ aws_cdk_intro_workshop/cdk-workshop/lib/cdk-workshop-stack.ts
@@ -2,6 +2,7 @@ import cdk = require('@aws-cdk/core');
 import lambda = require('@aws-cdk/aws-lambda');
 import apigw = require('@aws-cdk/aws-apigateway');
 import { HitCounter } from './hitcounter';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -20,6 +21,11 @@ export class CdkWorkshopStack extends cdk.Stack {
     // defines an API Gateway REST API resource backed by our "hello" function.
     new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: helloWithCounter.handler
+    });
+
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table
     });
   }
 }
--- aws_cdk_intro_workshop/cdk-workshop/lib/hitcounter.ts
+++ aws_cdk_intro_workshop/cdk-workshop/lib/hitcounter.ts
@@ -11,12 +11,16 @@ export class HitCounter extends cdk.Construct {
   /** allows accessing the counter function */
   public readonly handler: lambda.Function;
 
+  /** the hit counter table */
+  public readonly table: dynamodb.Table;
+
   constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
     super(scope, id);
 
     const table = new dynamodb.Table(this, 'Hits', {
       partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
     });
+    this.table = table;
 
     this.handler = new lambda.Function(this, 'HitCounterHandler', {
       runtime: lambda.Runtime.NODEJS_8_10,
