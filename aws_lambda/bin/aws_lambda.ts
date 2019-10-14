207f24c39fa0208e14c1b973d69b609438050eef
nabinno <nab@blahfe.com>
2019-10-14 15:13:00 +0900

    AWS Lambda - Run `cdk init app --language=typescript` on aws_lambda.

---
+++ aws_lambda/bin/aws_lambda.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import cdk = require('@aws-cdk/core');
+import { AwsLambdaStack } from '../lib/aws_lambda-stack';
+
+const app = new cdk.App();
+new AwsLambdaStack(app, 'AwsLambdaStack');
