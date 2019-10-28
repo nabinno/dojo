82ad60506d824b62f66cfa3bc5a4067963b0d598
nabinno <nab@blahfe.com>
2019-10-28 19:19:41 +0900

    [wip]AWS Lambda - Apply aws-sam-cli.

--- aws_lambda/bin/invoke-lambda.sh
+++ aws_lambda/bin/invoke-lambda.sh
@@ -5,13 +5,19 @@ if (($# != 1)); then
   exit 1
 fi
 
+if [ ! -f template.yaml ]; then
+  cdk synth AwsLambdaApigatewayStack --no-staging >template.yaml
+fi
+
 lambda_name="$1"
 lambda_id=$(
-  cdk synth AwsLambdaApigatewayStack --no-staging |
+  cat template.yaml |
     grep -1 "Type: AWS::Lambda::Function" |
     grep -E "${lambda_name}Lambda.*:" |
     sed -e "s/ *\(.*\):/\1/g"
 )
 
 sam local invoke ${lambda_id} \
-  --no-event
+  -v lambda/${lambda_name} \
+  --no-event \
+  --debug
