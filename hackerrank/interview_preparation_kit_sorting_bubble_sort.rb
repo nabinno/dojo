1cd763eedf00054441944f8a49deac2d394d0501
nabinno <nab@blahfe.com>
2019-11-29 22:24:52 +0900

    HackerRank - Submit Interview Preparation Kit - Sorting - Mark and Toys.

--- hackerrank/interview_preparation_kit_sorting_bubble_sort.rb
+++ hackerrank/interview_preparation_kit_sorting_bubble_sort.rb
@@ -11,7 +11,7 @@ def countSwaps(a)
   valid_a = a.sort
   swap_cnt = 0
 
-  until a == valid_a do
+  until a == valid_a
     a.reverse_each.with_index do |elem, i|
       i = a.size - i - 1
 
@@ -33,3 +33,43 @@ n = gets.to_i
 a = gets.rstrip.split(' ').map(&:to_i)
 
 countSwaps a
+
+##
+# Mark and Toys
+#
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+# Complete the maximumToys function below.
+def maximumToys(prices, k)
+  result = []
+
+  buyable_prices = prices.select { |price| k >= price }
+  1.upto(buyable_prices.size) do |i|
+    result += buyable_prices
+      .each_cons(i)
+      .to_a
+      .select { |c| k >= c.sum }
+  end
+
+  result.sort_by { |r| r.size }.last.size
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+nk = gets.rstrip.split
+
+n = nk[0].to_i
+
+k = nk[1].to_i
+
+prices = gets.rstrip.split(' ').map(&:to_i)
+
+result = maximumToys(prices, k)
+
+fptr.write(result)
+fptr.write("\n")
+
+fptr.close()
