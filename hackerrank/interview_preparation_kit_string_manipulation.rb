f430f4b33c93c61d5585ff13e9053db4a158c63c
nabinno <nab@blahfe.com>
2019-11-30 11:04:50 +0900

    HackerRank - Submit Interview Preparation Kit - String Manipulation - Alternating Characters.

--- hackerrank/interview_preparation_kit_string_manipulation.rb
+++ hackerrank/interview_preparation_kit_string_manipulation.rb
@@ -30,3 +30,42 @@ fptr.write(res)
 fptr.write("\n")
 
 fptr.close()
+
+##
+# Alternating Characters
+#
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+# Complete the alternatingCharacters function below.
+def alternatingCharacters(s)
+  previous_char = ""
+  cnt = 0
+
+  s.chars do |c|
+    if c == previous_char
+      cnt += 1
+    end
+
+    previous_char = c
+  end
+
+  cnt
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+q = gets.to_i
+
+q.times do |q_itr|
+  s = gets.to_s.rstrip
+
+  result = alternatingCharacters s
+
+  fptr.write result
+  fptr.write "\n"
+end
+
+fptr.close()
