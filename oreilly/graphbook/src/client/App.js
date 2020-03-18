34302d4c5f397cc63257ce6c96f214ba3a227ce9
nabinno <nab@blahfe.com>
2020-03-18 21:50:52 +0900

    O'Reilly - Submit "Hands-On Full-Stack Web Development with GraphQL and React" - 1.b.4. CSS with webpack.

--- oreilly/graphbook/src/client/App.js
+++ oreilly/graphbook/src/client/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../../assets/css/style.css';
 
 const posts = [
   {
--- oreilly/graphbook/webpack.client.config.js
+++ oreilly/graphbook/webpack.client.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const buildDirectory = 'dist';
 const outputDirectory = buildDirectory + '/client';
+
 module.exports = {
   mode: 'development',
   entry: './src/client/index.js',
@@ -24,7 +25,7 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       }
     ]
-  }, 
+  },
   devServer: {
     port: 3000,
     open: true
