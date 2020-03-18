bcf3100f229fea7ff37aff78f151c4fef7cf0b07
nabinno <nab@blahfe.com>
2020-03-18 22:19:58 +0900

    O'Reilly - Submit "Hands-On Full-Stack Web Development with GraphQL and React" - 1.b.5. Event handling and state updates with React.

--- oreilly/graphbook/src/client/App.js
+++ oreilly/graphbook/src/client/App.js
@@ -22,22 +22,45 @@ const posts = [
 
 export default class App extends Component {
   state = {
-    posts: posts
+    posts: posts,
+    postContent: ''
   }
 
-  // constructor(props) {
-  //   super(props);
-  //
-  //   this.state = {
-  //     posts：posts
-  //   };
-  // }
+  handlePostContentChange(event) {
+    this.setState({postContent: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const newPost = {
+      id: this.state.posts.length + 1,
+      text: this.state.postContent,
+      user: {
+        avatar: '/uploads/avatar3.png',
+        username: 'Fake User'
+      }
+    };
+    this.setState((prevState) => ({
+      posts: [newPost, ...prevState.posts],
+      postContent: ''
+    }));
+  }
 
   render() {
-    const { posts } = this.state;
+    const { posts, postContent } = this.state;
 
     return (
       <div className="container">
+        <div className="postForm">
+          <form onSubmit={this.handleSubmit}>
+            <textarea
+              value={postContent}
+              onChange={this.handlePostContentChange}
+              placeholder="Write your custom post!"
+            />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
         <div className="feed">
           {posts.map(
             (post, i) =>
