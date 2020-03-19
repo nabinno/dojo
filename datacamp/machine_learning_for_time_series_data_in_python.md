c4e75bef55616f5be9217b48f95b1b40ec16c557
nabinno <nab@blahfe.com>
2020-03-19 22:58:02 +0900

    O'Reilly - Submit "Hands-On Full-Stack Web Development with GraphQL and React" - 1.b.7. Production build with webpack.

--- datacamp/machine_learning_for_time_series_data_in_python.md
+++ datacamp/machine_learning_for_time_series_data_in_python.md
@@ -50,14 +50,20 @@ model = LinearSVC()
 model.fit(X, y)
 ```
 
-## Fitting a simple model: classification
-```python
-
-```
-
 ## Predicting using a classification model
 ```python
+# Create input array
+X_predict = targets[['petal length (cm)', 'petal width (cm)']]
 
+# Predict with the model
+predictions = model.predict(X_predict)
+print(predictions)
+
+# Visualize predictions and actual values
+plt.scatter(X_predict['petal length (cm)'], X_predict['petal width (cm)'],
+            c=predictions, cmap=plt.cm.coolwarm)
+plt.title("Predicted class values")
+plt.show()
 ```
 
 ## Fitting a simple model: regression
--- oreilly/graphbook/webpack.client.build.config.js
+++ oreilly/graphbook/webpack.client.build.config.js
@@ -4,42 +4,41 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const buildDirectory = 'dist';
 const outputDirectory = buildDirectory + '/client';
-
 module.exports = {
-  mode: 'production',
-  entry: './src/client/index.js',
-  output: {
-    path: path.join(__dirname, outputDirectory),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+    mode: 'production',
+    entry: './src/client/index.js',
+    output: {
+      path: path.join(__dirname, outputDirectory),
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [{ loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../'
+            }
+          }, 'css-loader'],
         }
-      },
-      {
-        test: /\.css$/,
-        use: [{ loader: MiniCssExtractPlugin.loader,
-                options: {
-                  publicPath: '../'
-                }
-              }, 'css-loader'],
-      }
+      ]
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+          cleanOnceBeforeBuildPatterns: [path.join(__dirname, buildDirectory)]
+        }),
+        new HtmlWebpackPlugin({
+          template: './public/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css',
+        })
     ]
-  },
-  plugins: [
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: [path.join(__dirname, buildDirectory)]
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'bundle.css',
-    })
-  ]
 };
