05a61acdf55e794b5aa30fc8f7820163bd1b55f8
nabinno <nab@blahfe.com>
2020-10-04 09:49:03 +0900

    LYHGG - Subscribe "2. Starting Out" - Boolean.

--- lyhgg/src/StartingOut.hs
+++ lyhgg/src/StartingOut.hs
@@ -4,7 +4,31 @@ module StartingOut
 
 startingOut :: IO ()
 startingOut = do
-  putStrLn (doubleNum 3)
+  putStrLn ""
+  putStrLn "# 2. Straight out"
+  doubleNum
+  putStrLn ""
+  boolean
 
-doubleNum :: Int -> String
-doubleNum x = show (x * x)
+doubleNum :: IO ()
+doubleNum = do
+  putStrLn "## Number"
+  putStrLn ("3 * 3: " <> (show (3 * 3)))
+
+boolean :: IO ()
+boolean = do
+  putStrLn "## Boolean"
+  put "True && False" (True && False)
+  put "True && True" (True && True)
+  put "False || True" (False || True)
+  put "not False" (not False)
+  put "not (True && True)" (not (True && True))
+  put "5 == 5" (5 == 5)
+  put "1 == 0" (1 == 0)
+  put "5 /= 5" (5 /= 5)
+  put "5 /= 4" (5 /= 4)
+  put "hello == hello" ("hello" == "hello")
+
+put :: String -> Bool -> IO ()
+put x y = do
+  putStrLn (x <> ": " <> (show y))
